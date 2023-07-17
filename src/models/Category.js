f5537a4b3359171f78ec700fc2e2ced09b45288d
Jefferson Santos <jeff-ds@outlook.com>
2023-07-17 12:57:48 -0300

    req07 - ok

---
+++ src/models/Category.js
@@ -0,0 +1,17 @@
+module.exports = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    underscored: true,
+    timestamps: false,
+    tableName: 'categories',
+  });
+
+  return Category;
+};
