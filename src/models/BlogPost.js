34b4c2947971fde78eb08a8e81bbf35ffb2668df
Jefferson Santos <jeff-ds@outlook.com>
2023-07-17 17:26:25 -0300

    req10 - ok

---
+++ src/models/BlogPost.js
@@ -0,0 +1,28 @@
+module.exports = (sequelize, DataTypes) => {
+  const BlogPost = sequelize.define('BlogPost', {
+    id: {
+      primaryKey: true,
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+    },
+    title: DataTypes.STRING,
+    content: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    published: DataTypes.DATE,
+    updated: DataTypes.DATE,
+  },
+  {
+    underscored: true,
+    timestamps: false,
+    tableName: 'blog_posts',
+  });
+
+  BlogPost.associate = (models) => {
+    BlogPost.belongsTo(models.User, {
+      foreignKey: 'userId',
+      as: 'user',
+    });
+  };
+
+  return BlogPost;
+};
--- src/models/User.js
+++ src/models/User.js
@@ -16,5 +16,12 @@ module.exports = (sequelize, DataTypes) => {
     tableName: 'users',
   });
 
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId',
+      as: 'blogPost',
+    });
+  };
+
   return User;
 };
