c2014e3a64d5c9ae5622f7de1392f1229db731cb
Jefferson Santos <jeff-ds@outlook.com>
2023-07-17 17:36:47 -0300

    req11 - ok

---
+++ src/models/PostCategory.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  },
+  {
+    underscored: true,
+    timestamps: false,
+    tableName: 'posts_categories',
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blog_posts',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
